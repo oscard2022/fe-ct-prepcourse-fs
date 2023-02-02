7e3c8b91a01d1fceb3d138dbd8fc17e14169f44f
oscar <oscargomezluna@gmail.com>
2023-02-02 18:47:58 -0500

    ejercicios 04

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,8 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+   return (Math.max(x,y) || x === y )
+      
 }
 
 function mayoriaDeEdad(edad) {
@@ -12,6 +14,12 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   if (edad >= 18){
+      return ("Allowed");
+   }
+   else (edad < 18)
+      return ("Not allowed");
+   
 }
 
 function conection(status) {
@@ -21,6 +29,15 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+   if (status === 1){
+      return ("Online");
+   }
+   else if (status === 2){
+      return ("Away");
+   }
+   else (status !==1 || status !==2 )
+      return ("Offline")
+      
 }
 
 function saludo(idioma) {
@@ -30,7 +47,18 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
-}
+   if (idioma === "aleman"){
+      return ("Guten Tag!");
+   } 
+   else if (idioma === "mandarin"){
+      return ("Ni Hao!");
+   }
+   else if (idioma === "ingles"){
+      return ("Hello!")
+   }
+   else ("")
+     return ("Hola!")
+ }
 
 function colors(color) {
    // La función recibe un color. Retornar el string correspondiente:
@@ -41,6 +69,19 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   switch (color){
+      case ("blue"):
+      return ("This is blue");
+      case ("red"):
+      return ("This is red");
+      case ("green"):
+      return ("This is green");
+      case ("orange"):
+      return ("This is orange");
+      default:
+      return ("Color not found");
+
+   }
 }
 
 function esDiezOCinco(num) {
